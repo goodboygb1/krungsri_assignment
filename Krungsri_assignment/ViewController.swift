a15bfe636fe5436dad231fec5ea8eb0c85a0b19e
Poom Wutthidech <wutthidech@cal-cal.com>
2022-08-19 19:06:02 +0700

    first commit

---
+++ Krungsri_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Krungsri_assignment
+//
+//  Created by PMJs on 19/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
